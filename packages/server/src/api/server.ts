969dd84a4d3436e8493b0cf63762061ace5a73dd
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-07-27 21:28:02 +0000

    removed useless "server.ts" from types

--- packages/server/src/api/server.ts
+++ packages/server/src/api/server.ts
@@ -1,32 +1,41 @@
-import { ServerTypes } from "../types/server";
 import express from "express";
 import type { Application, Request, Response } from "express";
 
 export namespace Server {
-    class Instance implements ServerTypes.Instance {
-        route(
-            method: ServerTypes.Method,
-            endpoint: string,
-            handler: ServerTypes.Handler
-        ) {
+    export enum Method {
+        GET,
+        POST,
+        PUT,
+        PATCH,
+        DELETE
+    }
+
+    export type Handler = (req: Request, res: Response)=>{};
+
+    export interface Options {
+        port: number;
+    }
+
+    class Instance {
+        route(method: Method, endpoint: string, handler: Handler) {
             const callback: express.RequestHandler = async (req, res) => {
                 await handler(req, res);
             };
             
             switch (method) {
-                case ServerTypes.Method.GET:
+                case Method.GET:
                     this.router.get(endpoint, callback);
                     break;
-                case ServerTypes.Method.POST:
+                case Method.POST:
                     this.router.post(endpoint, callback);
                     break;
-                case ServerTypes.Method.PUT:
+                case Method.PUT:
                     this.router.put(endpoint, callback);
                     break;
-                case ServerTypes.Method.PATCH:
+                case Method.PATCH:
                     this.router.patch(endpoint, callback);
                     break;
-                case ServerTypes.Method.DELETE:
+                case Method.DELETE:
                     this.router.delete(endpoint, callback);
                     break;
             }
@@ -38,12 +47,12 @@ export namespace Server {
 
         constructor(
             private app: Application,
-            public options: ServerTypes.Options,
+            public options: Options,
             private router = express.Router()
         ) {}
     }
 
-    export function createServer(options: ServerTypes.Options): ServerTypes.Instance {
+    export function createServer(options: Options): Instance {
         const app = express();
         const router = express.Router();
 
